e7ed748f2d2e630c8794d723950c2a1d7da01cba
Katia <puzynia.katia@gmail.com>
2022-12-05 19:17:51 +0300

    homework 6

---
+++ Task_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+double[] GetArray(int x)
+{
+    double[] arr = new double[x];
+
+    WriteLine("Введите числа по порядку");
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = Convert.ToDouble(ReadLine());
+    }
+
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Write(arr[i]);
+        if (i < arr.Length - 1)
+        {
+            Write(", ");
+        }
+    }
+}
+
+void CountDigits(double[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    WriteLine($"В заданной выборке {count} чисел больше нуля");
+}
+
+
+WriteLine("Сколько числе вы хотите ввести?");
+int length = Convert.ToInt32(ReadLine());
+
+double[] array = GetArray(length);
+PrintArray(array);
+WriteLine();
+CountDigits(array);
---
+++ Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+Clear();
+
+double GetDouble()
+{
+    return Convert.ToDouble(ReadLine());
+}
+
+WriteLine("Введите значение k1: ");
+double k1 = GetDouble();
+WriteLine("Введите значение k2: ");
+double k2 = GetDouble();
+WriteLine("Введите значение b1: ");
+double b1 = GetDouble();
+WriteLine("Введите значение b2: ");
+double b2 = GetDouble();
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if (k1 == k2)
+{
+    if (b1 == b2)
+    {
+        WriteLine("Прямые совпадают");
+    }
+    WriteLine("Прямые параллельны");
+}
+else
+{
+    WriteLine($"Точка пересечения ({x};{y})");
+}
+
